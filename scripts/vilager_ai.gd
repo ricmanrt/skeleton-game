d09178c7370eae9b32e00d7d66afa8fb1fdffa9c
MersMaer <ispolatovas@gmail.com>
2024-08-19 01:23:23 +0300

    Minor decoration fix and AI
    Map updated
    Started sorting out AI

--- scripts/vilager_ai.gd
+++ scripts/vilager_ai.gd
@@ -1,9 +1,5 @@
-#class_name GlobalEvents
 extends Node
 
-signal skeleton_pickup(position: Vector2)
-signal item_dropped(position: Vector2)
-signal health_gained(position: Vector2)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
