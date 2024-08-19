c0f521646694f8d718d52da99f9800d46b507af3
MersMaer <ispolatovas@gmail.com>
2024-08-19 19:51:27 +0300

    AI and Enemy placement
    Vilager AI done
    A few more enemies palce
    Map fixes

--- scripts/vilager_ai.gd
+++ scripts/vilager_ai.gd
@@ -1,11 +1,35 @@
+# Basic AI
 extends Node
 
+@onready var group: MinionGroup = $".."
 
-# Called when the node enters the scene tree for the first time.
+var target : Node2D
+
 func _ready() -> void:
-	pass # Replace with function body.
+	await group.ready
+	group.moving = false
+	group.near_area.body_entered.connect(_on_body_entered)
+	group.near_area.body_exited.connect(_on_body_exited)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if target == null:
+		group.moving = false
+	else: 
+		group.target.position = group.average_position.position + target.position.direction_to(group.average_position.position) * 10
+		group.moving = true
+
+func _on_body_entered(body: Node2D):
+	if body is Player:
+		target = body
+		Globals.debug_message = "run away form player"
+	elif target == null and body is Minion:
+		Globals.debug_message = "Found "+ body.name
+		var m = body as Minion
+		if m.damage_group == Globals.DamageGroups.SKELETONS:
+			target = body
+			Globals.debug_message = "got target bones"
+
+func _on_body_exited(body: Node2D):
+		target = null
+		group.moving = false
+		Globals.debug_message = "no target bones"
