3d2200c8837c62901e1308a1a5c34f0ce5048d59
ricmanrt <ricmanrt.comms@gmail.com>
2024-08-19 19:34:08 -0700

    Added a win screen scene

---
+++ scripts/win_screen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+var text = """
+The townsfolk have suffered for their arrogance and noise making. 
+
+your destruction is worth, hmm... %d points. Great job.
+"""
+@onready var win_label: Label = %WinLabel
+
+func _ready() -> void:
+	visibility_changed.connect(_on_visibility_changed)
+
+func _on_visibility_changed() -> void:
+	if (visible):
+		win_label.text = text % Globals.score
+		win_label.visible_ratio = 0
+		create_tween().tween_property(win_label,"visible_ratio",1.0,4.0)
